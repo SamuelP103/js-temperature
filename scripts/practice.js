445c7b0eb3b51938695a14ed1972fbf19f2dfcff
Samuel Price <sjp@Samuels-MBP.lan>
2023-12-13 22:08:57 -0500

    practiceHW

---
+++ scripts/practice.js
@@ -0,0 +1,96 @@
+/*console.log('hello world');
+
+let fName='samuel';
+let lName="price";
+let age=23; 
+
+
+console.log(fName);
+console.log(lName);
+console.log(age);
+
+let a=35
+let b=21
+
+console.log("mult: " +(a*b));
+console.log("div: " +(a/b));
+console.log("add: " +(a+b));
+console.log("sub: " +(a-b));
+
+
+
+let assignmentName = "Class 1 - Assingment 1";
+let description= "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+let pointsAs1 = 6.66;
+let pointsAs2 = 6.66;
+let pointsAs3 = 6.66;
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads= true;
+let studentAnnotation = false;
+
+let dueDate="12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);*/
+
+/*
+let assignmentName = "Class 1 - Assingment 1";
+let description= "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads= true;
+let studentAnnotation = false;
+
+let dueDate="12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);*/
+
+let notifications = 1;
+let storiesDisplayed = "billy bob, ben smith";
+let birthdays = "bobby smith and tom have birthdays;today.";
+let messageNotification = 1;
+let offline = true;
+let Adposition1 = "brave Dream(img)";
+let realsVideoDisplay = "lion king, friends";
+let likes = 5;
+let reposts = 0;
+let comments = 1;
+let peopleYouMayKnow = "billy jean, michael jackson, toby mac";
+let suggestedGroupToJoin = "the cool kid hangout";
+let postSuggestedToYou = "john got a job today!";
+let IsThisYou = "img";
+let pressPlay = "playButtonShow";
+let memoriesAYearAgo = "postApr132022";
+let postingWheel = true;
+let postedConfirmation = true;
+let postingConfirmationFailMessage = false;
+let SuggestedFriendShift = true;
+let shortViews = 22;
+let topPostReaction = "laugh";
+
+document.write(`<h1> ${IsThisYou} </h1>`);
+document.write(`<h1> ${messageNotification} </h1>`);
+document.write(`<h1> ${notifications} </h1>`);
+document.write(`<p> ${storiesDisplayed} </p>`);
+document.write(`<h2> ${postingWheel} </h2>`);
+document.write(`<h2> ${postedConfirmation} </h2>`);
+document.write(`<h2> ${postingConfirmationFailMessage} </h2>`);
+document.write(`<h2> ${memoriesAYearAgo} </h2>`);
+document.write(`<p> ${postSuggestedToYou} </p>`);
+document.write(`<p> ${topPostReaction} </p>`);
+document.write(`<p> ${reposts} </p>`);
+document.write(`<p>likes:  ${likes} </p>`);
+document.write(`<p>comments:  ${comments} </p>`);
+document.write(`<p> ${realsVideoDisplay} </p>`);
+document.write(`<p> ${shortViews} </p>`);
+document.write(`<p> ${pressPlay} </p>`);
+document.write(`<p> ${peopleYouMayKnow} </p>`);
+document.write(`<p> ${birthdays} </p>`);
+document.write(`<p> ${Adposition1} </p>`);
+document.write(`<p> ${suggestedGroupToJoin} </p>`);
+document.write(`<p> ${SuggestedFriendShift} </p>`);
+document.write(`<p> ${offline} </p>`);
+
+
