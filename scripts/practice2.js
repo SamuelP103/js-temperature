10aa07bd8fdafee5e084c3011e377123ed55018b
Samuel Price <sjp@Samuels-MBP.lan>
2023-12-15 14:22:47 -0500

    test

---
+++ scripts/practice2.js
@@ -0,0 +1,31 @@
+//console.log("Prac 2");
+//let myName=prompt("Enter your name:");
+//console.log(myName);
+/*
+function sum(){
+    let num1=prompt("enter your number");
+    let num2=prompt("number2");  
+    num3=parseInt(num2)+parseInt(num1);
+    console.log(num3);
+    //document.write(num3);
+    document.getElementById("solution").innerHTML=`<p> Total: ${num3} </p>`;
+    //document.getElementById("solution").innerHTML=num3;
+    //can do number instead of parseInt
+}
+function sub(){
+    let num4=prompt("enter your number");
+    let num5=prompt("number2");  
+    num6=parseInt(num4)-parseInt(num5);
+    console.log(num6);
+    //document.write(num6);
+    document.getElementById("solution").innerHTML=num6;
+    //can do number instead of parseInt
+}
+*/
+// Homework 2 starts here
+
+
+
+function ftc(){
+    let temp1=prompt(Fare)
+}
